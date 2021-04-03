58b8643bf0094f80343897e71a25521e97860511
Mahi2024 <77480600+Mahi2024@users.noreply.github.com>
2021-04-03 12:49:28 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+var ball,ballImg,bg;
+var database;
+
+function preload(){
+    bg = loadImage("bg.png");
+    ballImg = loadImage("ball.png");
+}
+
+function setup(){
+    createCanvas(1000,500);
+    database = firebase.database();
+    ball = createSprite(250,250,10,10);
+    ball.addImage(ballImg);
+    ball.scale = 0.6;
+    var lc = database.ref("Ball/position");
+    lc.on("value",readop);
+}
+
+function draw(){
+    background(bg);
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+    database.ref("Ball/position").set({
+        'x':position.x+x,
+        'y':position.y+y
+    })
+}
+
+function readop(data){
+    position = data.val();
+    ball.x = position.x;
+    ball.y = position.y;
+}
